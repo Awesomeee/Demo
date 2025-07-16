fca7c8003abbe89fe9a077c9e3931ca1bb7368b7
Awesomeee <duyle.computer@gmail.com>
2025-07-16 15:02:20 +0700

    Add security and endpoint file
    add more code to control SecurityFilterChain and a rest endpoint which deliver csrf token to use when developer want to send request via API in httpBasic mode

--- monolithic_shop_app/src/main/java/org/demo/monolithic_shop_app/security_module/SecurityConfiguration.java
+++ monolithic_shop_app/src/main/java/org/demo/monolithic_shop_app/security_module/SecurityConfiguration.java
@@ -13,11 +13,51 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 @Configuration
 public class SecurityConfiguration {
 
+	/*
+	 * author: duyl
+	 * created date: Wed 16 Jul, 2025
+	 * 
+	 * SIMPLE: turn off csrf in formLogin mode, COMPLEX: turn on csrf in httpBasic mode, COMPLEX_2: turn on csrf in formLogin mode
+	 * When use in formLogin, should turn off csrf because currently I still haven't known how to unlock it for rest client yet,
+	 * the only way so far is to set security configuration so that csrf will ignore the specific endpoint you want to call,
+	 * When use in httpBasic(In a microservice architecture where the service doesn't need to have form or user interaction), you
+	 * can turn on csrf and follow this process: first call a GET request to endpoint /csrf-token to get csrf-token value, then you
+	 * use the value of it to add a http header "X-CSRF-TOKEN" with the token value (of course we also need a Authorization httpHeader)
+	 */
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
-        http
+		String mode = "SIMPLE";		//SIMPLE: turn off csrf in formLogin mode, COMPLEX: turn on csrf in httpBasic mode, COMPLEX_2: turn on csrf in formLogin mode
+		if(mode == "SIMPLE") {
+	        http
+	            // Disable CSRF for H2 console
+	        	.csrf(csrf -> csrf.disable())	//disable csrf completely
+	            // Allow frames from the same origin (essential for H2 console)
+	            .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
+	            // Permit all requests to H2 console path
+	            .authorizeHttpRequests(auth -> auth
+	                .requestMatchers(antMatcher("/h2-console/**"), antMatcher("/guest/**"), antMatcher("/test/**")).permitAll()
+	                .anyRequest().authenticated() // Protect other endpoints
+	            )
+	            .formLogin(Customizer.withDefaults());
+		} else if (mode == "COMPLEX") {
+			http
+	            // Disable CSRF for H2 console
+	            .csrf(csrf -> csrf.ignoringRequestMatchers(antMatcher("/h2-console/**"), antMatcher("/test/**")))
+	        	//.csrf(csrf -> csrf.disable())	//disable csrf completely
+	            // Allow frames from the same origin (essential for H2 console)
+	            .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
+	            // Permit all requests to H2 console path
+	            .authorizeHttpRequests(auth -> auth
+	                .requestMatchers(antMatcher("/h2-console/**"), antMatcher("/guest/**"), antMatcher("/test/**")).permitAll()
+	                .anyRequest().authenticated() // Protect other endpoints
+	            )
+	            //.formLogin(Customizer.withDefaults());
+	            .httpBasic(Customizer.withDefaults());
+		} else if (mode == "COMPLEX_2") {
+			http
             // Disable CSRF for H2 console
             .csrf(csrf -> csrf.ignoringRequestMatchers(antMatcher("/h2-console/**"), antMatcher("/test/**")))
+        	//.csrf(csrf -> csrf.disable())	//disable csrf completely
             // Allow frames from the same origin (essential for H2 console)
             .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
             // Permit all requests to H2 console path
@@ -26,6 +66,7 @@ public class SecurityConfiguration {
                 .anyRequest().authenticated() // Protect other endpoints
             )
             .formLogin(Customizer.withDefaults());
+	}
         return http.build();
     }
 
---
+++ monolithic_shop_app/src/main/java/org/demo/monolithic_shop_app/security_module/SecurityRestfulApi.java
@@ -0,0 +1,17 @@
+package org.demo.monolithic_shop_app.security_module;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class SecurityRestfulApi {
+	
+	@GetMapping("/csrf-token")
+    public CsrfToken csrf(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+    }
+
+}
