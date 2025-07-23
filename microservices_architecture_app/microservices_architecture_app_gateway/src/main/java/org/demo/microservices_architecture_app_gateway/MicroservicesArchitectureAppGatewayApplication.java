5f269387b6617bfa14e7f58bc84f93d4dcd4c941
Awesomeee <duyle.computer@gmail.com>
2025-07-23 20:39:32 +0700

    Write logic router in gateway
    Write custom bean to define a gateway that forward web ui to angular server and forward web service to spring server

--- microservices_architecture_app/microservices_architecture_app_gateway/src/main/java/org/demo/microservices_architecture_app_gateway/MicroservicesArchitectureAppGatewayApplication.java
+++ microservices_architecture_app/microservices_architecture_app_gateway/src/main/java/org/demo/microservices_architecture_app_gateway/MicroservicesArchitectureAppGatewayApplication.java
@@ -21,27 +21,64 @@ public class MicroservicesArchitectureAppGatewayApplication {
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 	    return builder.routes()
-	        .route(p -> p
-	            .path("/aaa")
+	        /*.route(p -> p
+	            .path("/api/aaa")
 	            .filters(f -> f.addRequestHeader("Hello", "World")
 	            				.circuitBreaker(config -> config.setFallbackUri("forward:/fallback")))
 	            .uri("http://localhost:8080/aaa"))
 	        .route(p -> p
-		            .path("/aab")
+		            .path("/api/aab")
 		            .filters(f -> f.addRequestHeader("Hello", "World"))
 		            .uri("http://localhost:8080/aab"))
 	        .route(p -> p
-		            .path("/aac")
+		            .path("/api/aac")
 		            .filters(f -> f.addRequestHeader("Hello", "World"))
 		            .uri("http://localhost:8080/aac"))
 	        .route(p -> p
-		            .path("/aad")
+		            .path("/api/aad")
 		            .filters(f -> f.addRequestHeader("Hello", "World"))
 		            .uri("http://localhost:8080/aad"))
 	        .route(p -> p
-		            .path("/ccc")
+		            .path("/api/ccc")
 		            .filters(f -> f.addRequestHeader("Hello", "World"))
-		            .uri("http://localhost:8081/ccc"))
+		            .uri("http://localhost:8081/ccc"))*/
+	        /*.route(p -> p
+		            .path("/shop")
+		            .filters(f -> f.addRequestHeader("Hello", "World"))
+		            .uri("http://127.0.0.1:4200/"))*/
+	        /*.route(p -> p
+		            .path("/shop/**")
+		            .filters(f -> f.addRequestHeader("Hello", "World")
+		            		.rewritePath("/shop(?<segment>.*)", "/${segment}"))
+		            .uri("http://127.0.0.1:4200"))*/
+	        /*.route(p -> p
+		            .path("/polyfills.js")
+		            .filters(f -> f.addRequestHeader("Hello", "World"))
+		            .uri("http://127.0.0.1:4200/polyfills.js"))
+	        .route(p -> p
+		            .path("/main.js")
+		            .filters(f -> f.addRequestHeader("Hello", "World"))
+		            .uri("http://127.0.0.1:4200/main.js"))*/
+    		/*.route(p -> p
+		            .path("/**")
+		            .and()
+		            .not(np -> np.path("/api/**"))
+		            .filters(f -> f.addRequestHeader("Hello", "World")
+		            		)
+		            .uri("http://127.0.0.1:4200"))*/
+    		.route(p -> p
+    	            .path("/api/**")
+    	            .filters(f -> f.addRequestHeader("Hello", "World")
+    	            				.circuitBreaker(config -> config.setFallbackUri("forward:/fallback"))
+    	            				.rewritePath("/api/(?<segment>.*)", "/${segment}"))
+    	            .uri("http://localhost:8080"))
+    		.route(p -> p
+		            .path("/**")
+		            .and()
+		            .not(np -> np.path("/api/**"))
+		            .filters(f -> f.addRequestHeader("Hello", "World")
+		            		)
+		            .uri("http://127.0.0.1:4200"))
 	        .build();
 	}
 	
