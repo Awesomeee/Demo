55453ff114c128a299c5875416bfc77ead6de606
Awesomeee <duyle.computer@gmail.com>
2026-02-21 09:18:12 +0700

    Update testing code

--- monolithic_shop_app/src/test/java/org/demo/monolithic_shop_app/MonolithicShopAppApplicationTests.java
+++ monolithic_shop_app/src/test/java/org/demo/monolithic_shop_app/MonolithicShopAppApplicationTests.java
@@ -1,13 +1,22 @@
 package org.demo.monolithic_shop_app;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.demo.monolithic_shop_app.web_service_module.shop.ProductApi;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MonolithicShopAppApplicationTests {
+	
+	@Autowired
+	private ProductApi productApi;
 
 	@Test
 	void contextLoads() {
+		assertThat(productApi).isNotNull();
 	}
+	
 
 }
