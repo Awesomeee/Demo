b22d8fcd9810271b0a314f2a554aad903080c326
Awesomeee <duyle.computer@gmail.com>
2026-04-08 16:16:19 +0700

    Add household module to digital our home
    Added house_module package and HouseHoldController class

---
+++ monolithic_shop_app/src/main/java/org/demo/monolithic_shop_app/household_module/HouseHoldController.java
@@ -0,0 +1,44 @@
+package org.demo.monolithic_shop_app.household_module;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HouseHoldController {
+	
+	public String viewGuestRoom() {
+		return "ok";
+	}
+	
+	public String viewBedRoom() {
+		return "ok";
+	}
+	
+	public String viewKitchen() {
+		return "ok";
+	}
+	
+	public String viewAltarRoom() {
+		return "ok";
+	}
+	
+	public String viewBookShelf() {
+		return "ok";
+	}
+	
+	public String viewYard() {
+		return "ok";
+	}
+	
+	public String viewClothesCabinet() {
+		return "ok";
+	}
+	
+	public String viewKitchenCabinet() {
+		return "ok";
+	}
+	
+	public String viewToolBox() {
+		return "ok";
+	}
+
+}
